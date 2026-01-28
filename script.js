1684c8ac3fe1b173f405574c47ef9ec980959d60
Samad <Samad.o@outlook.com>
2026-01-28 15:30:04 -0500

    Can now swtich time format from military to civil

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const clock = document.getElementById("time");
+const format = document.getElementById("format-switch");
 
 let date;
 
-function currentTimeCivil() {
-  setInterval(() => {
-    date = new Date();
-    let dateString = date.toLocaleTimeString();
+let fetchCivil = true;
 
+let fetchMilitary = false;
+
+let miitary;
+
+let civil;
+
+function currentTimeCivil() {
+  if (!fetchCivil) return;
+
+  civil = setInterval(() => {
+    date = new Date();
+    dateString = date.toLocaleTimeString();
     clock.textContent = dateString;
   });
 }
+
 function currentTimeMilitary() {
-  setInterval(() => {
+  if (!fetchMilitary) return;
+
+  military = setInterval(() => {
     date = new Date();
 
     let ampm;
 
-    if (date.getHours >= 12) {
+    if (date.getHours() >= 12) {
       ampm = "PM";
     } else {
       ampm = "AM";
@@ -33,5 +46,19 @@ function currentTimeMilitary() {
     clock.textContent = dateTimeMilitary;
   });
 }
+
+format.addEventListener("click", () => {
+  if (fetchCivil) {
+    fetchCivil = false;
+    fetchMilitary = true;
+    clearInterval(civil);
+    currentTimeMilitary();
+  } else if (fetchMilitary) {
+    fetchMilitary = false;
+    fetchCivil = true;
+    clearInterval(military);
+    currentTimeCivil();
+  }
+});
 
 currentTimeMilitary();
