cb3f305ab34d34d8773e5c4dd281e63103d37c8e
Samad <Samad.o@outlook.com>
2026-01-28 00:18:18 -0500

    displays am now

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const clock = document.getElementById("time");
 
-function currentTime() {
+function currentTime() {}
+
+setInterval(() => {
   const date = new Date();
   let dateString = date.toLocaleTimeString();
 
-  dateTwoFour = dateString.split(" ")[0];
-
-  clock.textContent = dateTwoFour;
-}
-
-setInterval(currentTime);
+  clock.textContent = dateString;
+});
